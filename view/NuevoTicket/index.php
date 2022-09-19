c5620268a5b44d541ee963e050412be5e5f24fd0
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2022-09-19 10:12:08 -0600

    cambio nuevo ticket

--- view/NuevoTicket/index.php
+++ view/NuevoTicket/index.php
@@ -12,35 +12,37 @@
 				<p>
 					Desde esta ventana podr&aacute; generar nuevos tickets.
 				</p>
-				<div class="row">
+				
 					<form method='post' id='ticketForm'>
-						<div class="col-lg-12">
-							<fieldset class="form-group">
-								<input type="hidden" name='id_usuario' value="<?php echo $_SESSION["id"]?>">
-								<label class="form-label semibold" for="exampleInput">Categoria</label>
-								<select name="id_categoria" id="id_categoria" class='form-control'></select>
-							</fieldset>
-						</div>
-						<div class="col-lg-12">
-							<fieldset class="form-group">
-								<label class="form-label" for="exampleInputEmail1">Titulo</label>
-								<input type="text" class="form-control" id="titulo" name='titulo' 
-								placeholder="Ingresa el titulo">
-							</fieldset>
-						</div>
-						<div class="col-lg-12">
-							<fieldset class="form-group">
-								<label class="form-label" for="exampleInputPassword1">Descripci&oacute;n</label>
-								<div class="summernote-theme-1">
-									<textarea class="summernote" name="descripcion" id='descripcion'></textarea>
-								</div>
-								<div class="col-lg-12 text-center">
-									<button type='submit' name='action' value='Add' class="btn btn-rounded btn-inline btn-primary">Guardar</button>
-								</div>
-							</fieldset>
-						</div>
+						<div class="row">
+							<div class="col-lg-12">
+								<fieldset class="form-group">
+									<input type="hidden" name='id_usuario' value="<?php echo $_SESSION["id"]?>">
+									<label class="form-label semibold" for="exampleInput">Categoria</label>
+									<select name="id_categoria" id="id_categoria" class='form-control'></select>
+								</fieldset>
+							</div>
+							<div class="col-lg-12">
+								<fieldset class="form-group">
+									<label class="form-label" for="exampleInputEmail1">Titulo</label>
+									<input type="text" class="form-control" id="titulo" name='titulo' 
+									placeholder="Ingresa el titulo">
+								</fieldset>
+							</div>
+							<div class="col-lg-12">
+								<fieldset class="form-group">
+									<label class="form-label" for="exampleInputPassword1">Descripci&oacute;n</label>
+									<div class="summernote-theme-1">
+										<textarea class="summernote" name="descripcion" id='descripcion'></textarea>
+									</div>
+									<div class="col-lg-12 text-center">
+										<button type='submit' name='action' value='Add' class="btn btn-rounded btn-inline btn-primary">Guardar</button>
+									</div>
+								</fieldset>
+							</div>
+						</div><!--.row-->
 					</form>
-				</div><!--.row-->
+				
 			</div>
 		</div><!--.container-fluid-->
 	</div><!--.page-content-->
